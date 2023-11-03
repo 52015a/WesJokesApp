3bcf1708914d6d28bb2c903233d223e6aca0201d
52015 <52015@aebatalha.ccems.pt>
2023-11-03 17:13:02 +0000

    Adicionar arquivos de projeto.

---
+++ WesJokesApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WesJokesApp.Models;
+
+namespace WesJokesApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WesJokesApp.Models.Jokes> Jokes { get; set; } = default!;
+    }
+}
---
+++ WesJokesApp/Models/Jokes.cs
@@ -0,0 +1,18 @@
+﻿namespace WesJokesApp.Models
+{
+    public class Jokes
+    {
+        public int id { get; set; }
+        public string JokePergunta { get; set; }
+        public string JokeResposta { get; set; }
+        public string IdUser { get; set; }
+
+        public Jokes()
+        {
+            id = 0;
+            JokePergunta=string.Empty;
+            JokeResposta=string.Empty;
+            IdUser = string.Empty;
+        }
+    }
+}
